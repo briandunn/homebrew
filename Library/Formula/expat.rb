96532506ed8652b0f9d969ceb89510aeb77c7fbf
Brian Dunn <brianpatrickdunn@gmail.com>
2011-04-20 10:40:58 -0500

    add jibberish to the url to apease sourceforge

--- Library/Formula/expat.rb
+++ Library/Formula/expat.rb
@@ -1,7 +1,8 @@
 require 'formula'
 
 class Expat <Formula
-  url 'http://downloads.sourceforge.net/project/expat/expat/2.0.1/expat-2.0.1.tar.gz'
+  url 'http://downloads.sourceforge.net/project/expat/expat/2.0.1/expat-2.0.1.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fexpat%2Ffiles%2F&ts=1296248798&use_mirror=softlayer'
+  version '2.0.1'
   homepage 'http://expat.sourceforge.net/'
   md5 'ee8b492592568805593f81f8cdf2a04c'
 
